00caa3b6276f624ffe0a57b9031031a8fd45ee9a
osirota <oleh.sirota@gmail.com>
2024-09-08 12:48:06 +0300

    test

--- app/components/Participant.tsx
+++ app/components/Participant.tsx
@@ -1,14 +1,16 @@
 // components/Participant.tsx
 import React from 'react';
+import { useTelegram } from '@/app/lib/TelegramProvider';
 
 interface ParticipantProps {
   participant: string;
 }
 
 const Participant: React.FC<ParticipantProps> = ({ participant }) => {
+  const { user } = useTelegram();
   return (
     <div className="w-20 h-20 p-4 border border-red-700">
-      {/* <img src={avatar} alt="participant" className="w-full h-full rounded-md" /> */}
+      <img src={user?.photo_url} alt="participant" className="w-full h-full rounded-md" />
       {participant}
     </div>
   );
