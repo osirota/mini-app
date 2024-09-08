18cc848ac6c7f87575e3492d0b36530cda9d7664
osirota <oleh.sirota@gmail.com>
2024-09-08 12:50:16 +0300

    test

--- app/components/Roulette/index.tsx
+++ app/components/Roulette/index.tsx
@@ -49,18 +49,12 @@ const ParticipantAnimation: React.FC<ParticipantAnimationProps> = ({ participant
    <div className="flex flex-col">
       <div>
          <p className="text-cyan-500">auth_date: {webApp?.initDataUnsafe.auth_date}</p>
-            <p className="text-cyan-500 pl-5 pt-2">hash: {webApp?.initDataUnsafe.hash}</p>
-            <p className="text-cyan-500 pl-5 pt-2">query_id: {webApp?.initDataUnsafe.query_id}</p>
-            <p className="text-cyan-500 pl-5 pt-2">first_name: {webApp?.initDataUnsafe.user.first_name}</p>
-            <p className="text-cyan-500 pl-5 pt-2">user_id: {webApp?.initDataUnsafe.user.id}</p>
-            <p className="text-cyan-500 pl-5 pt-2">language_code: {webApp?.initDataUnsafe.user.language_code}</p>
-            <p className="text-cyan-500 pl-5 pt-2">last_name: {webApp?.initDataUnsafe.user.last_name}</p>
-            <p className="text-cyan-500 pl-5 pt-2">username: {webApp?.initDataUnsafe.user.username}</p>
             <p className="text-cyan-500 pl-5 pt-2">user:first_name: {user?.first_name}</p>
             <p className="text-cyan-500 pl-5 pt-2">user:id: {user?.id}</p>
             <p className="text-cyan-500 pl-5 pt-2">user:language_code: {user?.language_code}</p>
             <p className="text-cyan-500 pl-5 pt-2">user:last_name: {user?.last_name}</p>
             <p className="text-cyan-500 pl-5 pt-2">user:username: {user?.username}</p>
+            <p className="text-cyan-500 pl-5 pt-2">user:photo: {user?.photo_url}</p>
          </div>
          <div className="flex flex-col">
          <div className="relative w-96 h-24 overflow-hidden flex justify-center items-center">
